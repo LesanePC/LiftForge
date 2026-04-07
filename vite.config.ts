50a0397c578cd629ea41cd760d36c956afcce21b
Eugene Fokin <evgen94@bk.ru>
2026-04-07 20:36:22 +0300

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/LiftForge/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
 })
